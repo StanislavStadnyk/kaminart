1b458cf6848b8d8f2c6965a9e6c7378e11dc3a73
StanislavStadnyk <stanislav.stadnyk@gmail.com>
2024-05-10 13:26:07 +1000

    Updates

---
+++ precache-manifest.8b174e848ae115555fc2c8943965339d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b56e48ba52f88f4608b2b8e31ed5084",
+    "url": "/kaminart/index.html"
+  },
+  {
+    "revision": "09356e3246af6989aa01",
+    "url": "/kaminart/static/css/2.8ff17cee.chunk.css"
+  },
+  {
+    "revision": "7526cdb9909de7737378",
+    "url": "/kaminart/static/css/main.1b427c6f.chunk.css"
+  },
+  {
+    "revision": "09356e3246af6989aa01",
+    "url": "/kaminart/static/js/2.ed3534f7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kaminart/static/js/2.ed3534f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7526cdb9909de7737378",
+    "url": "/kaminart/static/js/main.735cda84.chunk.js"
+  },
+  {
+    "revision": "d1e076d0e9c93217e6ed",
+    "url": "/kaminart/static/js/runtime-main.884f9887.js"
+  }
+]);
